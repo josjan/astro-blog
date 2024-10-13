d5cf5f078d12f1a6e4f419befc854a513b01c2a3
josjan <josealamo@gmail.com>
2024-10-13 18:01:26 +0100

    First commit

---
+++ src/config/site-config.ts
@@ -0,0 +1,4 @@
+export const siteConfig = {
+    title: "Jose's Blog",
+    description: "Blog oficial de José Álamo - web developer"
+}
---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,30 @@
+import rss from '@astrojs/rss';
+import type { APIRoute } from 'astro';
+import { getCollection } from 'astro:content';
+
+export const GET: APIRoute = async ({ params, request, site }) => {
+
+    const blogPosts = await getCollection('blog')
+
+    return rss({
+        stylesheet: '/styles/rss.xsl',
+        // `<title>` field in output xml
+        title: 'Jose Blog',
+        // `<description>` field in output xml
+        description: 'Un simple blog sobre mis aventuras con astro',
+        // Pull in your project "site" from the endpoint context
+        // https://docs.astro.build/en/reference/api-reference/#contextsite
+        site: site ?? '',
+        // Array of `<item>`s in output xml
+        // See "Generating items" section for examples using content collections and glob imports
+        items: blogPosts.map(({data, slug}) => ({
+            title: data.title,
+            pubDate: data.date,
+            descripction: data.description,
+            link: `post/${slug}`,
+        })),
+        // (optional) inject custom xml
+        customData: `<language>es-es</language>`,
+      });
+}
+
